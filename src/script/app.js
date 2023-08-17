a05cff1e147b6900eb1f2f9337de647797a6249b
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-08-18 04:19:56 +0530

    Search Funtionality Logic

--- src/script/app.js
+++ src/script/app.js
@@ -1,4 +1,4 @@
-console.log("hello")
+console.log("hello");
 // Functionality
 //      Search Component
 
@@ -8,16 +8,45 @@ console.log("hello")
 //          Meteorite composition (recclass)
 //          Mass range (e.g. low to high, inclusive)
 
-// 1 => onload call a function which will fetch the data from api 
+// 1 => onload call a function which will fetch the data from api. DONE
 // 2 => store the result in a variable
+//      We have fetched the data and stored in a local storage. DONE
 // 3 => on search check the type of data we want to fetch (ex : Name,Year of strike,Meteorite composition (recclass),Mass range (e.g. low to high, inclusive))
+//      Receive data from input box
+//      check the type of data
 // 4 => if we found the result then show all the data in table format and if no result found with perticular query we show message "no result found"
 
-function fetchData(){
-    fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')   
-    .then(response => response.json())
-    .then(res =>console.log(res));
+
+// let dateSTr = "1880-01-01T00:00:00.000";
+// let date = new Date(dateSTr);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+// console.log(date.getMinutes());
+
+function fetchData() {
+  fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
+    .then((response) => response.json())
+    .then((res) => {
+      localStorage.setItem("response", JSON.stringify(res));
+    });
 }
 
-fetchData()
+let input = document.getElementById("inputSearchBar");
+input.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("form submitted");
+  let input = document.getElementById("inputBox");
+  let data = localStorage.getItem("response");
 
+  if (data != undefined) {
+    console.log(input);
+    const foundObject = obj.find((item) => item.name === input.value);
+    if (foundObject) {
+      console.log("Object found:", foundObject);
+    } else {
+      console.log("Object not found");
+    }
+  }
+});
